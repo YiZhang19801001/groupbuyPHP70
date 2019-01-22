48d1a96c18f301e7b70681571da2886506046540
yizhang19801001 <yizhang19801001@gmail.com>
2019-01-22 17:51:54 +1100

    etc

--- resources/assets/js/components/DemoCarousel.js
+++ resources/assets/js/components/DemoCarousel.js
@@ -4,19 +4,15 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 
 export default class DemoCarousel extends Component {
-    render() {
-        return (
-            <Carousel showThumbs={false}>
-                <div>
-                    <img src="/groupbuy/public/images/slide_1.png" />
-                </div>
-                <div>
-                    <img src="/groupbuy/public/images/slide_1.png" />
-                </div>
-                <div>
-                    <img src="/groupbuy/public/images/slide_1.png" />
-                </div>
-            </Carousel>
-        );
-    }
+  render() {
+    return (
+      <Carousel showThumbs={false}>
+        <img src="/groupbuy/public/images/slide_1.png" />
+
+        <img src="/groupbuy/public/images/slide_1.png" />
+
+        <img src="/groupbuy/public/images/slide_1.png" />
+      </Carousel>
+    );
+  }
 }
